3749be3e851ffd7dd81224692db620d5bebae2ec
Suman1901 <sumansahoo41243@gmail.com>
2025-01-16 06:13:18 +0000

    learn dataTypes

---
+++ basics/2-dataTypes.js
@@ -0,0 +1,19 @@
+// dataType list
+
+// (A) primitive dataTypes:-
+
+    // 1. number = 40/453 etc.
+    // 2. string = ""
+    // 3. boolean = true/ false
+    // 4. null = standalone value (empty)
+    // 5. undefined =  value is not define
+    // 6. symbol = unique
+    // 7. bigInt =
+
+
+// (B) NON-primitive dataTypes:-  
+    //  1. object
+
+
+
+console.log(typeof null);
